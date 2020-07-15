b3e036aacceca55f5bf97f60e4b334ec95de7fc4
Joseph Kalinda <68041786+CIGATECHNOLOGY@users.noreply.github.com>
2020-07-15 11:57:30 +0100

    upload
    updated from the team

---
+++ insc.php
@@ -0,0 +1,24 @@
+<?php 
+
+include("db_connect.php") ;
+$nom=trim($_POST['name']);
+$pnom=trim($_POST['lastname']);
+$mail=trim($_POST['email']);
+$message=trim($_POST['message']);
+$do = $req->fetch();
+if($nom!="" and $pnom!="" and $mail !="" and $message!="" ){
+$req = $bdd->prepare("INSERT INTO ciga_message (nom, pnom,email, message, datee) 
+							VALUES (:name,:pnom,:mail, :message, NOW()) ");
+		$req->execute(array(
+		"name" => $nom,
+        "pnom" =>$pnom,
+        "mail" =>$mail,
+        "message" =>$message,
+		));
+		header("location=index.html");
+}
+else{
+header("location=contact-us.html");
+}
+
+?>
